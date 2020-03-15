6ad2c2740f50724f5f901d2cf80e5c409a14bf4f
jheslop25 <john@earthmountain.ca>
2020-03-15 23:46:11 +0000

    added guzzle to project. began creating development interface for instacart wrapper

--- app/Http/Controllers/instacartController.php
+++ app/Http/Controllers/instacartController.php
@@ -3,15 +3,27 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use GuzzleHttp\Client;
+
 
 class instacartController extends Controller
 {
-    public function login(Request $request){
+    protected $baseURL = 'https://www.instacart.ca';
+
+    protected $cookieSessionName = "_instacart_session";
+
+
+    public function login(Request $request, $baseURL){
         $this->iCartEmail = $request->instacartEmail;
         $this->iCartPass = $request->instacartPassword;
+
+        $client = new Client([
+            'base_uri' => $baseURL,
+            'timeout' => 2.0
+        ]);
+
         // import guzzle and create http request to instacart
-        var_dump($this->iCartEmail);
-        var_dump($this->iCartPass);
+        return view('development.interface');
     }
 
     public function search(Request $request){
---
+++ resources/views/development/interface.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('content')
+    @auth
+        <div class="card m-5 p-4">this is the instacart development interface</div>
+    @endauth
+@endsection
