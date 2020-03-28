3462fc6ab622d4fa9b34c175a0f3cd024fd47849
jheslop25 <john@earthmountain.ca>
2020-03-28 02:56:34 +0000

    upgraded instacart login. it now returns the _instacart_session cookie and parses it. next step will be to test using the search end point

--- app/Http/Controllers/instacartController.php
+++ app/Http/Controllers/instacartController.php
@@ -4,7 +4,72 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
+use GuzzleHttp\Cookie\SetCookie as CookieParser;
 //use GuzzleHttp\RequestOptions;
+
+function parse_cookies($header) {
+
+	$cookies = array();
+
+	$cookie = new cookie();
+
+	$parts = explode("=",$header);
+	for ($i=0; $i< count($parts); $i++) {
+		$part = $parts[$i];
+		if ($i==0) {
+			$key = $part;
+			continue;
+		} elseif ($i== count($parts)-1) {
+			$cookie->set_value($key,$part);
+			$cookies[] = $cookie;
+			continue;
+		}
+		$comps = explode(" ",$part);
+		$new_key = $comps[count($comps)-1];
+		$value = substr($part,0,strlen($part)-strlen($new_key)-1);
+		$terminator = substr($value,-1);
+		$value = substr($value,0,strlen($value)-1);
+		$cookie->set_value($key,$value);
+		if ($terminator == ",") {
+			$cookies[] = $cookie;
+			$cookie = new cookie();
+		}
+
+		$key = $new_key;
+	}
+	return $cookies;
+}
+
+class cookie {
+	public $name = "";
+	public $value = "";
+	public $expires = "";
+	public $domain = "";
+	public $path = "";
+	public $secure = false;
+
+	public function set_value($key,$value) {
+		switch (strtolower($key)) {
+			case "expires":
+				$this->expires = $value;
+				return;
+			case "domain":
+				$this->domain = $value;
+				return;
+			case "path":
+				$this->path = $value;
+				return;
+			case "secure":
+				$this->secure = ($value == true);
+				return;
+		}
+		if ($this->name == "" && $this->value == "") {
+			$this->name = $key;
+			$this->value = $value;
+		}
+	}
+}
+
 
 
 class instacartController extends Controller
@@ -25,16 +90,16 @@ class instacartController extends Controller
         $this->iCartEmail = $request->instacartEmail;
         $this->iCartPass = $request->instacartPassword;
 
-        $client = Http::post($this->baseURL . '/accounts/login', [
-            'user' => [
+        $client = Http::post($this->baseURL . '/v3/dynamic_data/authenticate/login?source=mobile_web&cache_key=undefined', [
+            'scope' => [
                 'email' => $request->instacartEmail,
                 'password' => $request->instacartPassword
             ]
         ]);
-        var_dump($request->instacartPassword);
-        var_dump($request->instacartEmail);
-        var_dump($client->headers());
-        return view('development.interface',['body' => $client->status()]);
+        $cookies = parse_cookies($client->header('set-cookie'));
+        var_dump($cookies[4]->value);
+        $instaSession = $cookies;
+        return view('development.interface',['body' => '$instaSession']);
     }
 
     public function search(Request $request){
