968297f2b3b980b829aff336e387c5003d9ff7f0
jheslop25 <john@earthmountain.ca>
2020-03-30 01:33:31 +0000

    worked on methods in cart Controller. added function to store items in cart. use case: a user instantiates a cart but doesn't place order

--- app/Http/Controllers/cartController.php
+++ app/Http/Controllers/cartController.php
@@ -6,5 +6,32 @@ use Illuminate\Http\Request;
 
 class cartController extends Controller
 {
-    //
+    public function addList(Request $request){
+        //add an array of items to cart
+        if (Auth::check()){
+            $items = $request->items;
+            foreach($items as $item){
+                $cart = new \App\Cart();
+                $cart->user_id = Auth::user()->id;
+                $cart->ingredient_name = $item->name;
+                $cart->quantity = $item->quant;
+                $cart->unit = $item->unit;
+                $cart->save();
+            }
+
+        }
+    }
+
+    public function getCart(Request $request){
+        //get user's cart
+        if (Auth::check()){
+            $cart = \App\Cart::find(Auth::user()->id);
+
+            return response()->json(['cart' => $cart]);
+        }
+    }
+
+    public function emptyCart(){
+        //empty the user's cart
+    }
 }
