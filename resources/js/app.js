70da83ccc07d0e1c7e6270a3ae8e187765ff2015
jheslop25 <john@earthmountain.ca>
2020-03-31 21:41:14 +0000

    added vuetify to project. I have been to hell and back.

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,8 +5,10 @@ require('./bootstrap');
 window.Vue = require('vue');
 
 import Vuex from 'vuex';
+import Vuetify from 'vuetify/lib';
 
 Vue.use(Vuex);
+Vue.use(Vuetify);
 
 const store = new Vuex.Store({
     state: {},
@@ -16,10 +18,11 @@ const store = new Vuex.Store({
 });
 
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+Vue.component('app', require('./components/ExampleComponent.vue').default);
 
 
 
 const app = new Vue({
     el: '#app',
+    vuetify: new Vuetify(),
 });
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,5 @@
 const mix = require('laravel-mix');
+const VuetifyLoaderPlugin = require('vuetify-loader/lib/plugin');
 
 /*
  |--------------------------------------------------------------------------
@@ -12,4 +13,11 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-   .sass('resources/sass/app.scss', 'public/css');
+   .sass('resources/sass/app.scss', 'public/css')
+   .webpackConfig(webpack => {
+      return {
+          plugins: [
+            new VuetifyLoaderPlugin()
+          ]
+      };
+    });
