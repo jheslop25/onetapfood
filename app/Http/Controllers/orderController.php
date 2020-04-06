225e5065a234b3455ec9809a20cd9dcd5a3625df
jheslop25 <john@earthmountain.ca>
2020-04-06 03:17:33 +0000

    built functions in order Controller needs validation

--- app/Http/Controllers/orderController.php
+++ app/Http/Controllers/orderController.php
@@ -6,15 +6,38 @@ use Illuminate\Http\Request;
 
 class orderController extends Controller
 {
-    public function createOrder(){
+    public function createOrder(Request $request){
         // a function that allows the consumer/front end to create an order
+        $order = new \App\Order();
+        $order->user_id = $request->user()->id;
+        $order->cart_id = $request->input['cart'];
+        $order->content = $request->input['content']; //send valid json string
+        $order->status = 'new';
+        $order->save();
+
+        return response()->json(['msg'=>'order created'], 200);
+        //add some trigger here to notify store
     }
 
-    public function cancelOrder(){
+    public function cancelOrder(Request $request){
         // a fucntion for the user to cancel thier order
+        $order = \App\Order::where('user_id', $request->user()->id)->get();
+        $order->status = 'canceled';
+        $order->save();
+
+        return response()->json(['msg'=> 'order canceled'], 200);
     }
 
-    public function updateOrder(){
+    public function updateOrder(Request $request){
         // a function for the user to update their order
+        $order = \App\Order::where('user_id', $request->user()->id)->get();
+        $order->status = 'updated';
+        $order->content = $request->input['content'];
+        $order->save();
+    }
+
+    public function getById(Request $request){
+        $order = \App\Order::find($request->user()->id);
+        return response()->json(['order'=> $order], 200);
     }
 }
--- app/Order.php
+++ app/Order.php
@@ -11,7 +11,5 @@ class Order extends Model
     public function user(){
         return $this->belongsTo('\App\User');
     }
-    public function meal(){
-        return $this->hasMany('\App\Meal');
-    }
+
 }
