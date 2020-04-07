240e39f4c955e6efe142f5d4b999f296291f0fdb
jheslop25 <john@earthmountain.ca>
2020-04-07 03:05:49 +0000

    added auth to order controller

--- app/Http/Controllers/orderController.php
+++ app/Http/Controllers/orderController.php
@@ -3,46 +3,73 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class orderController extends Controller
 {
-    public function createOrder(Request $request){
+    public function createOrder(Request $request)
+    {
         // a function that allows the consumer/front end to create an order
-        $order = new \App\Order();
-        $order->user_id = $request->user()->id;
-        $order->cart_id = $request->input['cart'];
-        $order->content = $request->input['content']; //send valid json string
-        $order->status = 'new';
-        $order->save();
+        if (Auth::check()) {
+            $order = new \App\Order();
+            $order->user_id = $request->user()->id;
+            $order->cart_id = $request->input['cart'];
+            $order->content = $request->input['content']; //send valid json string
+            $order->status = 'new';
+            $order->save();
 
-        return response()->json(['msg'=>'order created'], 200);
+            return response()->json(['msg' => 'order created'], 200);
+        } else {
+            return response()->json(['msg' => 'please login'], 200);
+        }
         //add some trigger here to notify store
     }
 
-    public function cancelOrder(Request $request){
+    public function cancelOrder(Request $request)
+    {
         // a fucntion for the user to cancel thier order
-        $order = \App\Order::where('user_id', $request->user()->id)->get();
-        $order->status = 'canceled';
-        $order->save();
+        if (Auth::check()) {
+            $order = \App\Order::where('user_id', $request->user()->id)->get();
+            $order->status = 'canceled';
+            $order->save();
 
-        return response()->json(['msg'=> 'order canceled'], 200);
+            return response()->json(['msg' => 'order canceled'], 200);
+        } else {
+            return response()->json(['msg' => 'please login'], 200);
+        }
     }
 
-    public function updateOrder(Request $request){
+    public function updateOrder(Request $request)
+    {
         // a function for the user to update their order
-        $order = \App\Order::where('user_id', $request->user()->id)->get();
-        $order->status = 'updated';
-        $order->content = $request->input['content'];
-        $order->save();
+        if (Auth::check()) {
+            $order = \App\Order::where('user_id', $request->user()->id)->get();
+            $order->status = 'updated';
+            $order->content = $request->input['content'];
+            $order->save();
+        } else {
+            return response()->json(['msg' => 'please login'], 200);
+        }
     }
 
-    public function getById(Request $request){
-        $order = \App\Order::find($request->user()->id);
-        return response()->json(['order'=> $order], 200);
+    public function getById(Request $request)
+    {
+        if (Auth::check()) {
+            $order = \App\Order::find($request->user()->id);
+            return response()->json(['order' => $order], 200);
+        } else {
+            return response()->json(['msg' => 'please login'], 200);
+        }
     }
 
-    public function getByStatus(Request $request){
-        $order = \App\Order::where('user_id', $request->user()->id)->where('status', $request->input['status'])->get();
-        return response()->json(['order'=> $order], 200);
+    public function getByStatus(Request $request)
+    {
+        if (Auth::check()) {
+            $order = \App\Order::where('user_id', $request->user()->id)->where('status', $request->input['status'])->get();
+            return response()->json(['order' => $order], 200);
+        } else {
+            return response()->json(['msg' => 'please login'], 200);
+        }
     }
 }
+
