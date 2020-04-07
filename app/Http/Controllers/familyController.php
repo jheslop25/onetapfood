551be848d1c9ce1dbca5fff12009788097ef1aca
jheslop25 <john@earthmountain.ca>
2020-04-07 02:54:16 +0000

    added auth to family controller

--- app/Http/Controllers/familyController.php
+++ app/Http/Controllers/familyController.php
@@ -7,38 +7,59 @@ use Illuminate\Http\Request;
 
 class familyController extends Controller
 {
-    public function create(Request $request){
+    public function create(Request $request)
+    {
         // a function to store user's information about the user's family in the DB
-
-        $member = new Family();
-        $member->user_id = $request->user()->id;
-        $member->member_age_group = $request->input['ageGroup'];
-        $member->member_diet = $request->input['diet'];
-        $member->member_pref = $request->input['pref'];
-        $member->save();
-        return response()->json(['msg'=> 'member stored'], 200);
+        if (Auth::check()) {
+            $member = new Family();
+            $member->user_id = $request->user()->id;
+            $member->member_age_group = $request->input['ageGroup'];
+            $member->member_diet = $request->input['diet'];
+            $member->member_pref = $request->input['pref'];
+            $member->save();
+            return response()->json(['msg' => 'member stored'], 200);
+        } else {
+            return response()->json(['msg' => 'please login'], 200);
+        }
     }
 
-    public function update(Request $request){
+    public function update(Request $request)
+    {
         // a function to update family members or their preferences
-        $member = \App\Family::find($request->input['id']);
-        $member->user_id = $request->user()->id;
-        $member->member_age_group = $request->input['ageGroup'];
-        $member->member_diet = $request->input['diet'];
-        $member->member_pref = $request->input['pref'];
-        $member->save();
+        if (Auth::check()) {
+            $member = \App\Family::find($request->input['id']);
+            $member->user_id = $request->user()->id;
+            $member->member_age_group = $request->input['ageGroup'];
+            $member->member_diet = $request->input['diet'];
+            $member->member_pref = $request->input['pref'];
+            $member->save();
 
-        return response()->json(['msg'=> 'member updated'], 200);
+            return response()->json(['msg' => 'member updated'], 200);
+        } else {
+            return response()->json(['msg' => 'please login'], 200);
+        }
     }
 
-    public function destroy(Request $request){
+    public function destroy(Request $request)
+    {
         // a sinister function to destroy a family record
+        if (Auth::check()) {
+            Family::find($request->input['id'])->delete();
+            return response()->json(['msg' => 'member destroyed'], 200);
+        } else {
+            return response()->json(['msg' => 'please login'], 200);
+        }
         Family::find($request->input['id'])->delete();
-        return response()->json(['msg'=> 'member destroyed'], 200);
+        return response()->json(['msg' => 'member destroyed'], 200);
     }
 
-    public function get(Request $request){
-        $family = \App\Family::where('user_id', $request->user()->id)->get();
-        return response()->json(['family' => $family], 200);
+    public function get(Request $request)
+    {
+        if (Auth::check()) {
+            Family::find($request->input['id'])->delete();
+            return response()->json(['msg' => 'member destroyed'], 200);
+        } else {
+            return response()->json(['msg' => 'please login'], 200);
+        }
     }
 }
