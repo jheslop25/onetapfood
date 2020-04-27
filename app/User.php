5aa4f9c246df3119657ca4eee6b95f308e8f5b76
jheslop25 <john@earthmountain.ca>
2020-04-27 00:59:59 +0000

    added laravel cashier and completed basic config

--- app/User.php
+++ app/User.php
@@ -5,11 +5,13 @@ namespace App;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Cashier\Billable;
 use Laravel\Passport\HasApiTokens;
 
 class User extends Authenticatable
 {
     use HasApiTokens, Notifiable;
+    use Billable;
 
     /**
      * The attributes that are mass assignable.
