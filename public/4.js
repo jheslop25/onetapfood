aa219f11e86bc9b6933191d8182d408b716110d2
John Heslop <johnheslop24@gmail.com>
2020-05-06 20:14:38 +0000

    fixed layout of login screen

--- public/4.js
+++ public/4.js
@@ -17,13 +17,21 @@ __webpack_require__.r(__webpack_exports__);
 //
 //
 //
+//
+//
+//
+//
+//
+//
+//
 /* harmony default export */ __webpack_exports__["default"] = ({
   name: "Login",
   data: function data() {
     return {
-      email: '',
-      password: '',
-      error: ''
+      email: "",
+      password: "",
+      error: "",
+      showErrorBox: false
     };
   },
   methods: {
@@ -31,17 +39,18 @@ __webpack_require__.r(__webpack_exports__);
       var _this = this;
 
       var context = this;
-      axios.post('/api/user/login', {
+      axios.post("/api/user/login", {
         email: this.email,
         password: this.password
       }).then(function (result) {
-        localStorage.setItem('user-token', result.data.access_token.accessToken);
-        localStorage.setItem('user-id', result.data.user.id);
-        context.$router.push('/main');
+        localStorage.setItem("user-token", result.data.access_token.accessToken);
+        localStorage.setItem("user-id", result.data.user.id);
+        context.$router.push("/main");
         _this.$store.state.loggedIn = true;
       })["catch"](function (err) {
         console.log(err);
         context.error = "Sorry, something went wrong. We couldn't log you in.";
+        context.showErrorBox = true;
       });
     }
   }
@@ -65,36 +74,58 @@ var render = function() {
   var _h = _vm.$createElement
   var _c = _vm._self._c || _h
   return _c(
-    "v-form",
-    { ref: "form", staticClass: "card m-5 p-3" },
+    "v-row",
+    { attrs: { justify: "center" } },
     [
-      _c("p", { staticClass: "h6 red--text" }, [_vm._v(_vm._s(_vm.error))]),
-      _vm._v(" "),
-      _c("v-text-field", {
-        attrs: { label: "email" },
-        model: {
-          value: _vm.email,
-          callback: function($$v) {
-            _vm.email = $$v
-          },
-          expression: "email"
-        }
-      }),
-      _vm._v(" "),
-      _c("v-text-field", {
-        attrs: { label: "password" },
-        model: {
-          value: _vm.password,
-          callback: function($$v) {
-            _vm.password = $$v
-          },
-          expression: "password"
-        }
-      }),
-      _vm._v(" "),
-      _c("v-btn", { attrs: { color: "sucesss" }, on: { click: _vm.login } }, [
-        _vm._v("Login")
-      ])
+      _c(
+        "v-col",
+        { attrs: { xs: "12", sm: "8", md: "4", lg: "4" } },
+        [
+          _c(
+            "v-form",
+            { ref: "form", staticClass: "card p-3" },
+            [
+              _c("v-card-title", [_vm._v("Welcome, Please Login.")]),
+              _vm._v(" "),
+              _vm.showErrorBox
+                ? _c("v-alert", { attrs: { type: "error" } }, [
+                    _vm._v(_vm._s(_vm.error))
+                  ])
+                : _vm._e(),
+              _vm._v(" "),
+              _c("v-text-field", {
+                attrs: { label: "email" },
+                model: {
+                  value: _vm.email,
+                  callback: function($$v) {
+                    _vm.email = $$v
+                  },
+                  expression: "email"
+                }
+              }),
+              _vm._v(" "),
+              _c("v-text-field", {
+                attrs: { label: "password" },
+                model: {
+                  value: _vm.password,
+                  callback: function($$v) {
+                    _vm.password = $$v
+                  },
+                  expression: "password"
+                }
+              }),
+              _vm._v(" "),
+              _c(
+                "v-btn",
+                { attrs: { color: "sucesss" }, on: { click: _vm.login } },
+                [_vm._v("Login")]
+              )
+            ],
+            1
+          )
+        ],
+        1
+      )
     ],
     1
   )
@@ -120,9 +151,12 @@ __webpack_require__.r(__webpack_exports__);
 /* empty/unused harmony star reexport *//* harmony import */ var _node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../../../../node_modules/vue-loader/lib/runtime/componentNormalizer.js */ "./node_modules/vue-loader/lib/runtime/componentNormalizer.js");
 /* harmony import */ var _node_modules_vuetify_loader_lib_runtime_installComponents_js__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ../../../../node_modules/vuetify-loader/lib/runtime/installComponents.js */ "./node_modules/vuetify-loader/lib/runtime/installComponents.js");
 /* harmony import */ var _node_modules_vuetify_loader_lib_runtime_installComponents_js__WEBPACK_IMPORTED_MODULE_3___default = /*#__PURE__*/__webpack_require__.n(_node_modules_vuetify_loader_lib_runtime_installComponents_js__WEBPACK_IMPORTED_MODULE_3__);
-/* harmony import */ var vuetify_lib_components_VBtn__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! vuetify/lib/components/VBtn */ "./node_modules/vuetify/lib/components/VBtn/index.js");
-/* harmony import */ var vuetify_lib_components_VForm__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! vuetify/lib/components/VForm */ "./node_modules/vuetify/lib/components/VForm/index.js");
-/* harmony import */ var vuetify_lib_components_VTextField__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(/*! vuetify/lib/components/VTextField */ "./node_modules/vuetify/lib/components/VTextField/index.js");
+/* harmony import */ var vuetify_lib_components_VAlert__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! vuetify/lib/components/VAlert */ "./node_modules/vuetify/lib/components/VAlert/index.js");
+/* harmony import */ var vuetify_lib_components_VBtn__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! vuetify/lib/components/VBtn */ "./node_modules/vuetify/lib/components/VBtn/index.js");
+/* harmony import */ var vuetify_lib_components_VCard__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(/*! vuetify/lib/components/VCard */ "./node_modules/vuetify/lib/components/VCard/index.js");
+/* harmony import */ var vuetify_lib_components_VGrid__WEBPACK_IMPORTED_MODULE_7__ = __webpack_require__(/*! vuetify/lib/components/VGrid */ "./node_modules/vuetify/lib/components/VGrid/index.js");
+/* harmony import */ var vuetify_lib_components_VForm__WEBPACK_IMPORTED_MODULE_8__ = __webpack_require__(/*! vuetify/lib/components/VForm */ "./node_modules/vuetify/lib/components/VForm/index.js");
+/* harmony import */ var vuetify_lib_components_VTextField__WEBPACK_IMPORTED_MODULE_9__ = __webpack_require__(/*! vuetify/lib/components/VTextField */ "./node_modules/vuetify/lib/components/VTextField/index.js");
 
 
 
@@ -146,7 +180,11 @@ var component = Object(_node_modules_vue_loader_lib_runtime_componentNormalizer_
 
 
 
-_node_modules_vuetify_loader_lib_runtime_installComponents_js__WEBPACK_IMPORTED_MODULE_3___default()(component, {VBtn: vuetify_lib_components_VBtn__WEBPACK_IMPORTED_MODULE_4__["VBtn"],VForm: vuetify_lib_components_VForm__WEBPACK_IMPORTED_MODULE_5__["VForm"],VTextField: vuetify_lib_components_VTextField__WEBPACK_IMPORTED_MODULE_6__["VTextField"]})
+
+
+
+
+_node_modules_vuetify_loader_lib_runtime_installComponents_js__WEBPACK_IMPORTED_MODULE_3___default()(component, {VAlert: vuetify_lib_components_VAlert__WEBPACK_IMPORTED_MODULE_4__["VAlert"],VBtn: vuetify_lib_components_VBtn__WEBPACK_IMPORTED_MODULE_5__["VBtn"],VCardTitle: vuetify_lib_components_VCard__WEBPACK_IMPORTED_MODULE_6__["VCardTitle"],VCol: vuetify_lib_components_VGrid__WEBPACK_IMPORTED_MODULE_7__["VCol"],VForm: vuetify_lib_components_VForm__WEBPACK_IMPORTED_MODULE_8__["VForm"],VRow: vuetify_lib_components_VGrid__WEBPACK_IMPORTED_MODULE_7__["VRow"],VTextField: vuetify_lib_components_VTextField__WEBPACK_IMPORTED_MODULE_9__["VTextField"]})
 
 
 /* hot reload */
--- resources/js/MainApp/views/Login.vue
+++ resources/js/MainApp/views/Login.vue
@@ -1,37 +1,53 @@
 <template>
-  <v-form class="card m-5 p-3" ref="form" >
-      <p class="h6 red--text">{{ error }}</p>
-      <v-text-field v-model="email" label="email"></v-text-field>
-      <v-text-field v-model="password" label="password"></v-text-field>
-      <v-btn color="sucesss" @click="login">Login</v-btn>
-  </v-form>
+  <v-row justify="center">
+    
+    <v-col xs="12" sm="8" md="4" lg="4">
+      <v-form class="card p-3" ref="form">
+        <v-card-title>Welcome, Please Login.</v-card-title>
+        <v-alert v-if="showErrorBox" type="error">{{error}}</v-alert>
+        <v-text-field v-model="email" label="email"></v-text-field>
+        <v-text-field v-model="password" label="password"></v-text-field>
+        <v-btn color="sucesss" @click="login">Login</v-btn>
+      </v-form>
+    </v-col>
+    
+  </v-row>
 </template>
 <script>
 export default {
   name: "Login",
-  data(){
-      return {
-          email: '',
-          password: '',
-          error: ''
-      }
+  data() {
+    return {
+      email: "",
+      password: "",
+      error: "",
+      showErrorBox: false
+    };
   },
   methods: {
-      login: function () {
-          let context = this;
-           axios.post('/api/user/login', {
-                email: this.email,
-                password: this.password
-            }).then((result) => {
-                localStorage.setItem('user-token', result.data.access_token.accessToken);
-                localStorage.setItem('user-id', result.data.user.id);
-                context.$router.push('/main');
-                this.$store.state.loggedIn = true;
-            }).catch((err) => {
-                console.log(err);
-                context.error = "Sorry, something went wrong. We couldn't log you in.";
-            });
-      }
+    login: function() {
+      let context = this;
+      axios
+        .post("/api/user/login", {
+          email: this.email,
+          password: this.password
+        })
+        .then(result => {
+          localStorage.setItem(
+            "user-token",
+            result.data.access_token.accessToken
+          );
+          localStorage.setItem("user-id", result.data.user.id);
+          context.$router.push("/main");
+          this.$store.state.loggedIn = true;
+        })
+        .catch(err => {
+          console.log(err);
+          context.error =
+            "Sorry, something went wrong. We couldn't log you in.";
+          context.showErrorBox = true;
+        });
+    }
   }
 };
 </script>
